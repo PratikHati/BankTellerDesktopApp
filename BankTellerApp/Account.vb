93edfd7d83312abb90dc823261c28bec2ea1a305
Pratik Hati <pratikhati.work@gmail.com>
2022-12-10 20:25:27 +0530

    Add project files.

---
+++ BankTellerApp/Account.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports System.IO
+
+Public Class Account
+
+    Public Property AccountId As String
+    Public Property AccountName As String
+    Public Property Balance As Decimal
+    Public Property FilePath As String
+    Public Property ErrorMessage As String
+    Public Property Records As List(Of Account)
+
+
+    Private _totalDeposite As Decimal
+
+    Private _totalWithdraw As Decimal
+
+    Public ReadOnly Property TotalDeposite() As Decimal
+
+        Get
+            Return _totalDeposite
+        End Get
+    End Property
+
+    Public ReadOnly Property TotalWithraw() As Decimal
+
+        Get
+            Return _totalWithdraw
+        End Get
+    End Property
+
+    Public Sub New()
+        AccountName = String.Empty
+        Balance = 0D
+        Records = New List(Of Account)
+
+    End Sub
+
+    Public Function GetData(acctid As String) As Boolean
+
+        Dim infile As StreamReader = Nothing
+        ErrorMessage = String.Empty
+
+        Try
+            infile = File.OpenText(FilePath)
+            While Not infile.EndOfStream
+                Dim line As String = infile.ReadLine()
+
+                Dim fields() As String = line.Split(","c)
+
+                If acctid = fields(0) Then
+                    AccountId = fields(0)
+                    AccountName = fields(1)
+                    Balance = CDec(fields(2))
+                    _totalDeposite = 0D
+                    _totalWithdraw = 0D
+                    Return True
+                End If
+            End While
+            ErrorMessage = "Account " + acctid + " Not found"
+            Return False
+        Catch ex As Exception
+            ErrorMessage = ex.Message
+            Return False
+        Finally
+            If infile IsNot Nothing Then
+                infile.Close()
+            End If
+        End Try
+
+    End Function
+
+
+    Public Function GetData() As Boolean
+
+        Dim infile As StreamReader = Nothing
+        ErrorMessage = String.Empty
+
+        Dim fields() As String
+
+        Try
+            infile = File.OpenText(FilePath)
+            While Not infile.EndOfStream
+                Dim line As String = infile.ReadLine()
+
+                fields = line.Split(","c)
+
+                Dim account As Account
+                account.AccountId = fields(0)
+                account.AccountName = fields(1)
+                account.Balance = CDec(fields(2))
+
+                Records.Add(account)
+            End While
+
+            If Records Is Nothing Then
+                ErrorMessage = "File Not found"
+                Return False
+            End If
+
+            Return True
+        Catch ex As Exception
+            ErrorMessage = ex.Message
+            Return False
+        Finally
+            If infile IsNot Nothing Then
+                infile.Close()
+            End If
+        End Try
+
+    End Function
+
+    Public Sub Deposite(amount As Decimal)
+        _totalDeposite += amount
+        Balance += amount
+
+    End Sub
+
+    Public Function WithDrwaw(amount As Decimal) As Boolean
+
+        If Balance >= amount Then
+            _totalWithdraw += amount
+            Balance -= amount
+            Return True
+        Else
+            ErrorMessage = "Balance is too low to withdraw"
+            Return False
+        End If
+    End Function
+End Class
---
+++ BankTellerApp/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.IO
+
+Public Class frmBankTellerApp
+
+    Private currentAccount As Account
+    Private ReadOnly filePath As String = "../../accounts.dat"
+
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If currentAccount.GetData() Then
+            Using writer As New StreamWriter(filePath, False)
+                For i As Integer = 0 To currentAccount.Records.Count - 1
+                    If currentAccount.AccountId = currentAccount.Records(i).AccountId Then
+                        writer.WriteLine(currentAccount.AccountId & "," & currentAccount.AccountName & "," & currentAccount.Balance.ToString())
+                    Else
+                        writer.WriteLine(currentAccount.Records(i).AccountId & "," & currentAccount.Records(i).AccountName & "," & currentAccount.Records(i).Balance.ToString())
+                    End If
+
+                Next
+            End Using
+        Else
+            MessageBox.Show(currentAccount.ErrorMessage, "Error")
+            Clear()
+        End If
+    End Sub
+
+    Private Sub btnFind_Click(sender As Object, e As EventArgs) Handles btnFind.Click
+        currentAccount = New Account()
+        currentAccount.FilePath = filePath
+
+        If currentAccount.GetData(txtAccountNum.Text) Then
+            lblAccountName.Text = currentAccount.AccountName
+            lblBalance.Text = currentAccount.Balance.ToString("c")
+            btnDeposit.Enabled = True
+            btnWithdraw.Enabled = True
+        Else
+            MessageBox.Show(currentAccount.ErrorMessage, "Error")
+            Clear()
+        End If
+    End Sub
+
+    Private Sub Clear()
+        lblAccountName.Text = String.Empty
+        lblBalance.Text = String.Empty
+        btnDeposit.Enabled = False
+        btnWithdraw.Enabled = False
+    End Sub
+
+    Private Sub btnDeposit_Click(sender As Object, e As EventArgs) Handles btnDeposit.Click
+        Try
+            currentAccount.Deposite(CDec(txtAmount.Text))
+            lblBalance.Text = currentAccount.Balance.ToString("c")
+        Catch ex As Exception
+            MessageBox.Show("Please enter a numeric amount", "Error")
+
+        End Try
+    End Sub
+
+    Private Sub btnWithdraw_Click(sender As Object, e As EventArgs) Handles btnWithdraw.Click
+        Try
+            If currentAccount.WithDrwaw(CDec(txtAmount.Text)) Then
+                lblBalance.Text = currentAccount.Balance.ToString("c")
+            Else
+                MessageBox.Show(currentAccount.ErrorMessage, "Error")
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(currentAccount.ErrorMessage, "Error")
+
+        End Try
+    End Sub
+
+    Private Sub btnTotals_Click(sender As Object, e As EventArgs) Handles btnTotals.Click
+        MessageBox.Show($"Total deposites = {currentAccount.TotalDeposite}" &
+                        $"Total withdrawals = {currentAccount.TotalWithraw}")
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+
+
+End Class
